07e131d25b22f2db85988289ade36ba7c4bb6955
Timo Noko <timonoko@users.noreply.github.com>
2024-09-17 09:13:54 +0300

    --

--- openscad_tweaker_in_emacs.el
+++ openscad_tweaker_in_emacs.el
@@ -1,6 +1,7 @@
-;; If you untick the "Bring window to front after automatic reload" in Advanced
-;; you can increase and decrease number at cursor point with <F8> and <F7> buttons.
-;; Thus you adjust things realtime on screen like in real CAD-program.
+;; You can increase and decrease number at cursor point with F8 and F7 buttons.
+;; If you untick the "Bring window to front after automatic reload" in
+;; OpenSCAD Preferences-Advanced,
+;; you can thus adjust things realtime on screen like in real CAD-program.
 
 (defun increment-number-at-point ()
       (interactive)
